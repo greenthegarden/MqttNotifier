7bd9feb984e3ec6c2979c7013c9b310d06fe1139
Philip Cutler <cutlervufamily@gmail.com>
2015-04-25 20:35:40 +0930

    Create MqttNotifier.py

---
+++ MqttNotifier.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc) :
+
+  print("Connected with result code "+str(rc))
+
+  # Subscribing in on_connect() means that if we lose the connection and
+  # reconnect then subscriptions will be renewed.
+  #client.subscribe("$SYS/#")
+  
+  # general topics
+  client.subscribe("all/contoller/dst")
+
+  # pibot topics
+  client.subscribe("pibot/motor/#")
+  client.subscribe("pibot/controller/#")
+  client.subscribe("pibot/tool/#")
+  
+  # relayduino topics
+  client.subscribe("relayduino/status/#")
+  client.subscribe("relayduino/request/#")
+  client.subscribe("relayduino/control/#")
+  
+  # lcd display topics
+  client.subscribe("lcddisplay/status/#")
+  
+  # weather station topics
+  client.subscribe("weather/status/#")
+  client.subscribe("weather/measurement/#")
+  client.subscribe("weather/sunairplus/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg) :
+  print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60) # address of broker, broker port, 
+
+client.loop_forever()
